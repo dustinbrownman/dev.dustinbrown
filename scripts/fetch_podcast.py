60e3417aa38a8c34cbb4247355aab735ce4a8117
Dustin Brown <dbrown9@gmail.com>
2023-08-19 20:09:15 +0000

    Might have actually gotten the json structure right this time

--- scripts/fetch_podcast.py
+++ scripts/fetch_podcast.py
@@ -8,43 +8,49 @@ func = modal.Function.lookup("corise-podcast-project", "process_podcast")
 
 results = func.remote(feed_url, '/content/podcast/')
 
-podcast_details = results.get("podcast_details", False)
+details = results.get("podcast_details", False)
+summary = results.get("podcast_summary", False)
+guests = results.get("podcast_guests", False)
+highlights = results.get("podcast_highlights", False)
 
-if (not podcast_details):
+if (not details):
     print("No podcast details found")
     exit()
 
-filename = "-".join(f"{podcast_details['podcast_title']} {podcast_details['episode_title']}.json".lower().split(" "))
+filename = "-".join(f"{details['podcast_title']} {details['episode_title']}.json".lower().split(" "))
 
 template = f"""---
-title: {podcast_details['podcast_title']} | {podcast_details['episode_title']}
+title: {details['podcast_title']} | {details['episode_title']}
 date: {date.today()}
 ---
 
-![episode image]({podcast_details['episode_image']})
-
-{podcast_details['podcast_summary']}
-
-## Guests
+![episode image]({details['episode_image']})
 
 """
 
-for guest in podcast_details["podcast_guests"]:
-    guest_output = f"- {guest['name']}"
+if (summary):
+    template += f"## Summary\n\n"
 
-    if (guest.get("title", False)):
-        guest_output += f" - _{guest['title']}_"
+if (guests):
+    template += f"## Guests\n\n"
 
-    if (guest.get("organization", False)):
-        guest_output += f" - {guest['organization']}"
+    for guest in guests:
+        guest_output = f"- {guest['name']}"
 
-    if (guest.get("wiki", False)):
-        guest_output += f"[Wikipedia]({guest['wiki']['url']})"
+        if (guest.get("title", False)):
+            guest_output += f" - _{guest['title']}_"
 
-    template += f"{guest_output}\n"
+        if (guest.get("organization", False)):
+            guest_output += f" - {guest['organization']}"
 
-template += f"\n## Highlights\n\n"
-template += f"{podcast_details['highlights']}\n"
+        if (guest.get("wiki", False)):
+            guest_output += f"[Wikipedia]({guest['wiki']['url']})"
+
+        template += f"{guest_output}\n"
+
+if (highlights):
+    template += f"\n## Highlights\n\n"
+    template += f"{highlights}\n"
 
 new_file = open(f"../content/podcasts/{filename}", "w")
 
