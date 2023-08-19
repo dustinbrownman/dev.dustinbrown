866503add6323ed270454a1ab17b635abf34c2b3
Dustin Brown <dbrown9@gmail.com>
2023-08-19 19:12:41 +0000

    Add script for fetching podcasts

---
+++ scripts/fetch_podcast.py
@@ -0,0 +1,49 @@
+import sys
+import modal
+import json
+from datetime import date
+
+feed_url = sys.argv[0]
+
+func = modal.Function.lookup("corise-podcast-project", "process_podcast")
+
+output = func.call(feed_url, '/content/podcast/')
+
+podcast_details = json.loads(output)
+
+filename = f"{podcast_details['podcast_title']} {podcast_details['episode_title']}.json".split(" ").join("-")
+
+template = f"""---
+title: {podcast_details['episode_title']}
+date: {date.today()}
+---
+
+![episode image]({podcast_details['episode_image']})
+
+{podcast_details['podcast_summary']}
+
+## Guests
+
+"""
+
+for guest in podcast_details["guests"]:
+    guest_output = f"- {guest['name']}"
+
+    if (guest["title"]):
+        guest_output += f" - _{guest['title']}_"
+
+    if (guest["organization"]):
+        guest_output += f" - {guest['organization']}"
+
+    if (guest["wiki"]):
+        guest_output += f"[Wikipedia]({guest['wiki']['url']})"
+
+    template += f"{guest_output}\n"
+
+template += f"\n## Highlights\n\n"
+template += f"{podcast_details['highlights']}\n"
+
+new_file = open(f"../content/podcasts/{filename}", "w")
+
+new_file.write(template)
+new_file.close()
